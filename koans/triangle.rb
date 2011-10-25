0aba952e45519a1856a0060a023ed88aa7a82d4a
konradmarcinczyk <coonradoo@o2.pl>
2011-10-25 19:08:46 +0200

    exceptions completed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError, "Sides have to be greater than zero" if (a == 0) | (b == 0) | (c == 0)
+  raise TriangleError, "Sides have to be a postive number" if (a < 0) | (b < 0) | (c < 0)
+  raise TriangleError, "Two sides can never be less than the sum of one side" if ((a + b) < c) | ((a + c) < b) | ((b + c) < a)
+  raise TriangleError, "Two sides can never be equal one side" if ((a + b) == c) | ((a + c) == b) | ((b + c) == a)
+  return :equilateral if (a == b) & (b == c) & (a == c)
+  return :isosceles if (a == b) | (b == c) | (a == c)
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
